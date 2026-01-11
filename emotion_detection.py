94cb3ef33dd7286a220bf31ff5ad0dc779b1fd42
Peter Froehlich <peter_froehlich_de@outlook.com>
2026-01-11 02:59:09 -0500

    application creation

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,8 @@
+import requests
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = { "raw_document": { "text": text_to_analyse } }
+    my_obj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = my_obj, headers=header)  
+    return response.text
